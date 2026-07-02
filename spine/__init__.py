87aca06b1a7779b7d495d82d8c610c18c487ae06
James Ding <git@twango.dev>
2026-07-02 03:14:00 -0700

    chore: annotate version lines for release-please

--- spine/__init__.py
+++ spine/__init__.py
@@ -3,5 +3,5 @@
 from spine.config import ModelConfig, SpineConfig
 from spine.model import Spine
 
-__version__ = "0.1.0"
+__version__ = "0.1.0"  # x-release-please-version
 __all__ = ["Spine", "ModelConfig", "SpineConfig"]
